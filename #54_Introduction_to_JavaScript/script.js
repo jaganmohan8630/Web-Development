045a7ae0865a2b9d7fb411385fa5511428c7d4f3
Jagan Mohan Jangam <jaganmohanjangam9@gmail.com>
2024-12-01 18:45:12 +0530

    Jagan Mohan

---
+++ #54_Introduction_to_JavaScript/script.js
@@ -0,0 +1,18 @@
+alert("Hey there I  started learning JavaScript")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
+// console.error
+// console.warn
+var a=prompt("Enter your number")
+var isTrue = confirm("Are you sure you want to leave this page")
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is blasting")
+}
+
+console.log("Your number is " + a)
+document.title="Hello..."
+document.body.style.backgroundColor="yellowgreen"
