df6e22dbc32301e9c32b980a72ae638685485ecc
Jagan Mohan Jangam <jaganmohanjangam9@gmail.com>
2024-12-03 21:32:46 +0530

    Jagan Mohan

---
+++ #56_JavaScript_if_else/index.js
@@ -0,0 +1,23 @@
+console.log("I am Conditional Tutorial");
+
+let age=16;
+let grace=2;
+age += grace
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace);
+
+if(age>18){
+    console.log("You drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+let a=5;
+let b=15;
+c=a>b?(a-b):(b-a);
+console.log(c)
---
+++ #57_JavaScript_Loops/index.js
@@ -0,0 +1,44 @@
+console.log("I am a tutorial on loops");
+
+let a=1;
+
+// for (let i = 0; i < 20; i++) {
+//     console.log(a+i);                 -->for loop
+// }
+
+
+
+// for (const key in object) {
+//     if (Object.prototype.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+//                                      -->forin loop
+//     }
+// }
+//foreach loop
+
+
+// let obj={
+//     name:"Jagan Mohan",
+//     role:"Student"
+// }
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element);            -->forin loop
+//         console.log(key);
+//         console.log(element);         
+//     }
+// for (const h of "Jagan") {
+//     console.log(h);                      -->forof loop
+// }
+
+// let h=0;
+// while(h<10){
+//     console.log(h);                    -->while loop
+//     h++;
+// }
+
+let h=0;
+do{
+    console.log(h);                     //-->dowhile loop
+    h++;
+} while(h<5);
